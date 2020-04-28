317d76992e8cb4077063bdcab35c1fb9bed16ad1
djclayton21 <clayton.daniel.j@gmail.com>
2020-04-28 13:53:41 -0600

    typography setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla modi nobis
+    aliquid. Voluptas quae qui facere numquam. Dolorem, alias. Minus aliquid,
+    non possimus rem debitis nisi eos recusandae obcaecati rerum!
   </Layout>
 )
 
